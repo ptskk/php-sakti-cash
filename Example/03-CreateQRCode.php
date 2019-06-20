4948494d46241c1a135b761596d697c7cd7d1728
Aris Winardi <ariswinardy@gmail.com>
2019-06-20 16:03:29 +0700

    update EXAMPLE

--- Example/03-CreateQRCode.php
+++ Example/03-CreateQRCode.php
@@ -2,22 +2,21 @@
 require_once('../Sakticash.php');
 
 Sakticash_Config::$isProduction = false;
-Sakticash_Config::$usernameKey = "SzIvUmM2-bGkzWGdG-WmVES1lZ-bE14aldQ-VExtODNM-SlNpMXY5-OXRzay93-MD0=";
-Sakticash_Config::$passwordKey = "YUtWRE00-YWUrRGtY-bHJxMnFV-cVpXakdC-U1FoT3JZ-RWhHdkd4-cEtGVmN5-VT0=";
+Sakticash_Config::$usernameKey = "<<CHANGE WITH YOUR MERCHANT USERNAME KEY>>";
+Sakticash_Config::$passwordKey = "<<CHANGE WITH YOUR MERCHANT PASSWORD KEY>>";
 Sakticash_Config::$auth = base64_encode(Sakticash_Config::$usernameKey . ":" . Sakticash_Config::$passwordKey);
 
 $isConnected = Sakticash_Merchant::isConnected();
 
 if ($isConnected) {
-    $trasaction_id = '11111114';
+    $trasaction_id = rand(11111111, 99999999);
     $data = [
-        'amount' => 154300, //in IDR
+        'amount' => rand(10000, 100000), // fill in the price of the item purchased
         'transaction_id' => $trasaction_id, // must unique on merchant and API server
         'transaction_datetime' => date('Y-m-d H:i:s'),
     ];
 
-    $dataSigned = Sakticash_Merchant::createSignedData($data);
-    var_dump(Sakticash_Merchant::createQRCode($dataSigned));
+    var_dump(Sakticash_Merchant::createQRCode($data));
 
 } else {
     var_dump($isConnected);
--- Example/04-CheckStatus.php
+++ Example/04-CheckStatus.php
@@ -2,8 +2,8 @@
 require_once('../Sakticash.php');
 
 Sakticash_Config::$isProduction = false;
-Sakticash_Config::$usernameKey = "SzIvUmM2-bGkzWGdG-WmVES1lZ-bE14aldQ-VExtODNM-SlNpMXY5-OXRzay93-MD0=";
-Sakticash_Config::$passwordKey = "YUtWRE00-YWUrRGtY-bHJxMnFV-cVpXakdC-U1FoT3JZ-RWhHdkd4-cEtGVmN5-VT0=";
+Sakticash_Config::$usernameKey = "<<CHANGE WITH YOUR MERCHANT USERNAME KEY>>";
+Sakticash_Config::$passwordKey = "<<CHANGE WITH YOUR MERCHANT PASSWORD KEY>>";
 Sakticash_Config::$auth = base64_encode(Sakticash_Config::$usernameKey . ":" . Sakticash_Config::$passwordKey);
 
 $isConnected = Sakticash_Merchant::isConnected();
